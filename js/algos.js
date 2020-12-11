80db5ca5fd02210b851591f0d100b7600018e48c
LindaPhi <linda.phinouvong@le-campus-numerique.fr>
2020-12-11 18:46:01 +0100

    Display distance berween Grenoble and CityX

--- js/algos.js
+++ js/algos.js
@@ -3,15 +3,25 @@ Number.prototype.toRadians = function() {
   return this * Math.PI / 180;
 };
 
-
 // Calculates the distance between Grenoble and the given city
 function distanceFromGrenoble(city)
 {
-  console.log("distanceFromGrenoble - implement me !");
+
   var GrenobleLat = 45.166667;
   var GrenobleLong = 5.716667;
 
-  return 1;
+  const R = 6371e3; // metres
+  const φ1 = GrenobleLat * Math.PI/180; // φ, λ in radians
+  const φ2 = city.latitude * Math.PI/180;
+  const Δφ = (city.latitude-GrenobleLat) * Math.PI/180;
+  const Δλ = (city.longitude-GrenobleLong) * Math.PI/180;
+
+  const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+  return R * c; // in metres
 }
 
 // Swap 2 values in array csvData
